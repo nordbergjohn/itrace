c824ef57dd499a4408b8cc98fbc0a4a136c32920
nordberg.john@gmail.com <nordberg.john@gmail.com>
2020-09-06 19:45:58 +0200

    Added partial/full test to Makefile

--- Makefile
+++ Makefile
@@ -9,8 +9,11 @@ test_prog:
 	objcopy --only-keep-debug bin/test_prog dbg/test_prog.dbg
 	strip -g bin/test_prog
 
-run_test: cov test_prog
+run_partial_coverage_test: cov test_prog
 	./bin/itrace bin/test_prog | addr2line -e dbg/test_prog.dbg -f -p | grep -v ? | uniq
+
+run_full_coverage_test: cov test_prog
+	./bin/itrace bin/test_prog 1 2 3 | addr2line -e dbg/test_prog.dbg -p | grep -v ? | uniq
 
 clean:
 	rm -rf bin/itrace bin/test_prog dbg/
--- src/main.c
+++ src/main.c
@@ -6,6 +6,17 @@
 #include <sys/reg.h>
 #include <unistd.h>
 
+static inline void printInstructionPointer(pid_t child)
+{
+  struct user_regs_struct regs;
+  ptrace(PTRACE_GETREGS, child, NULL, &regs);
+#ifdef __x86_64__
+  printf("%llx\n", regs.rip);
+#else
+  printf("%lx\n", regs.eip);
+#endif
+}
+
 int main(int argc, char **argv)
 {
   if (argc < 2)
@@ -21,7 +32,7 @@ int main(int argc, char **argv)
     // child process
     char* prog = argv[1];
     ptrace(PTRACE_TRACEME, 0, NULL, NULL);
-    execl(prog, prog, NULL);
+    execvp(prog, &argv[2]);
   }
   else
   {
@@ -32,10 +43,7 @@ int main(int argc, char **argv)
       wait(&status);
       if(WIFEXITED(status))
         break; 
-      
-      struct user_regs_struct regs;
-      ptrace(PTRACE_GETREGS, child, NULL, &regs);
-      printf("%llx\n", regs.rip); 
+      printInstructionPointer(child);
       ptrace(PTRACE_SINGLESTEP, child, NULL, NULL);
     }
   }
